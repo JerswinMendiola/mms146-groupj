adf5a1207bd67a17f61b16e4665f74151e1b11ae
cccastro4 <cccastro4@up.edu.ph>
2025-08-08 00:49:26 +0800

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,1 +1,21 @@
-Class 
+class player:
+    def __init__(self, player_name: str):
+        ''' 
+        initialization of a new Player object
+        '''
+        self._player_name = player_name
+
+    def get_name(self):
+        '''
+        Returns the name of the player
+        '''
+        return self._player_name 
+    
+    def set_name(self, new_name: str):
+        '''
+        Updates the name of the player
+
+        Parameters:
+        new_name (str): The new name to assign to the player
+        '''
+        self.__player_name = new_name
