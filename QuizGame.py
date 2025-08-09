db067228a959c99443f618636c2d8cf35be5806f
itriedfr <jbmendiola1@up.edu.ph>
2025-08-09 19:09:00 +0800

    Added functions to QuizGame class.

---
+++ QuizGame.py
@@ -0,0 +1,25 @@
+class QuizGame:
+    '''
+    This class handles the quiz game logic.
+    '''
+    def __init__(self,questions):
+        ''' 
+        initialization of a new Player object
+        '''
+        self.__question_bank = questions
+        self.__score = 0
+
+    def display_question(self):
+        pass
+
+    def check_answer(self):
+        pass
+
+    def get_score(self):
+        pass
+
+    def shuffle_question_bank(self):
+        pass
+
+    def reset_game(self):
+        pass
