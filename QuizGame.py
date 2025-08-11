76fee4c349a4f4b34f0f8c1b78cd191302249c86
Joemar Tibes <jltibes@up.edu.ph>
2025-08-12 00:31:00 +0800

    Update QuizGame.py

--- QuizGame.py
+++ QuizGame.py
@@ -2,39 +2,72 @@ class QuizGame:
     '''
     This class handles the quiz game logic.
     '''
-    def __init__(self,questions):
+    def __init__(self, questions):
         ''' 
         This initializes the quiz game class. It will make a list of questions and the player's score variable.
         '''
-        self.__question_bank = questions
-        self.__score = 0
+        self.__question_bank = questions            #List of question(list)
+        self.__score = 0                            #Current player score(int)
+        self.__curent_questions_index = 0           #Index of the current question (int)
 
     def display_question(self):
         ''' 
         This displays a selected question to the user.
         '''
-        pass
-
-    def check_answer(self):
+       if self.__current_question_index < len(self.__question_bank):
+           question = self.__question_bank[self.__current_question_index]
+           print(f"\nQuestions {self.__current_question_index + 1}/{len(self.__question_bank)}")
+           print(question.text)
+           for i, option in enumerate(question.options, 1):
+               print(f"{i}. {option}")
+            return question.text, question.options
+        else:
+            print("No more questions available.")
+        
+    def check_answer(self, user_answer):
         ''' 
         This checks if the user chose the correct answer for a selected question.
         '''
-        pass
+        if self.__current_question_index < len(self.__question_bank):
+            question = self.__question_bank[self.__current_question_index]
+            is_correct = user_answer.lower() == question.correct_answer.lower()
+        if is_correct:
+            self.__score =+ 1
+            print("Correct!")
+        else:
+            print(f"Wrong! The Correct answer is: {question.correct_answer}")
 
     def get_score(self):
          ''' 
         This retrieves the score of the player in the current game.
         '''
-        pass
+        return self.__score, len(self.__question_bank)
 
     def shuffle_question_bank(self):
         ''' 
         This randomizes the questions order.
         '''
-        pass
+        random.shuffle(self.__question_bank)
+        print("Questions have been shuffled.")
 
     def reset_game(self):
         ''' 
         This resets the current game.
         '''
-        pass
+        self.__score = 0
+        self.__current_question_index = 0
+
+    def is_game_over (self):
+        '''
+        Checks if all questions have been answered.
+        '''
+        return self.__current_question_index >= len(self.__questions_bank)
+
+    def display_final_results(self):
+        '''
+        Displays the final results of the quiz
+        '''
+        total_score = self.get_score()
+        print("/n" + "="*40)
+        print("Quiz Completed!")
+        print(f"Your Final Score: {score}/{total}')
